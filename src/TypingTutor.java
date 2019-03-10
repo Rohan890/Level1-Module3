3d0580f28163f1ceaa1bcc55f0045aff179d8d0e
alvincvms <league@wts7.attlocal.net>
2019-03-10 15:54:29 -0700

    TypingTutor

--- src/TypingTutor.java
+++ src/TypingTutor.java
@@ -1,16 +1,20 @@
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class TypingTutor {
+public class TypingTutor implements KeyListener {
+	char currentLetter;
+	JLabel jLabel = new JLabel();
+	JPanel panel = new JPanel();
 public static void main(String[] args) {
 	TypingTutor t = new TypingTutor();
 	t.CreateUI();
-	char currentLetter;
-
-	currentLetter = generateRandomLetter();
-
+}
 	char generateRandomLetter() {
 	      Random r = new Random();
 	      return (char) (r.nextInt(26) + 'a');
@@ -19,10 +23,38 @@ public static void main(String[] args) {
 
 void CreateUI() {
 	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
 	frame.setTitle("Type or Die");
-	frame.pack();
+
 	frame.setVisible(true);
+	currentLetter = generateRandomLetter();
+	jLabel.setFont(jLabel.getFont().deriveFont(28.0f));
+	jLabel.setHorizontalAlignment(JLabel.CENTER);
+	jLabel.setText(currentLetter + "");
+	panel.add(jLabel);
+	frame.add(panel);
+	frame.addKeyListener(this);
+	frame.pack();
+}
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stu
+	if(e.getKeyChar() == currentLetter) {
+		panel.setBackground(Color.green);
+	}
+	else {
+		panel.setBackground(Color.red);
+	}
+	currentLetter = generateRandomLetter();
+	jLabel.setText(currentLetter + "");
+}
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
 	
 }
 }
